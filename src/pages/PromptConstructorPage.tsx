fc53fdd48eaff950df41f4a662684075909ec4cc
Sarin Suriyakoon <pacosarin@gmail.com>
2024-03-15 17:33:04 +0700

    add label for copied

--- src/pages/PromptConstructorPage.tsx
+++ src/pages/PromptConstructorPage.tsx
@@ -16,32 +16,37 @@ const PromptConstructorPage = () => {
         detail: '',
         outputFormat: '',
     });
-
+    const [copyState, setCopyState] = useState<boolean>(false)
+    const [copyXMLState, setCopyXMLState] = useState<boolean>(false)
     const concatenatedText = `Role: ${formState.role}\nInstruction: ${formState.instruction}\nDetail: ${formState.detail}\nOutput Format: ${formState.outputFormat}`;
     const concatenatedTextXML = `<Role>${formState.role}</Role>\n<Instruction>${formState.instruction}</Instruction>\n<Detail>${formState.detail}</Detail>\n<OutputFormat>${formState.outputFormat}</OutputFormat>`;
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        
+
         const { id, value } = e.target;
         setFormState((prevState) => ({ ...prevState, [id]: value }));
-      };
+        setCopyState(false)
+        setCopyXMLState(false)
+    };
     const copyToClipboard = () => {
-        navigator.clipboard.writeText(concatenatedText)
-            .then(() => {
-                alert('Text copied to clipboard!');
-            })
-            .catch((err) => {
-                console.error('Failed to copy text: ', err);
-            });
+        try {
+            navigator.clipboard.writeText(concatenatedText)
+            setCopyState(true)
+        }
+        catch (error) {
+            console.log(error)
+        }
     };
     const copyXMLToClipboard = () => {
-        navigator.clipboard.writeText(concatenatedTextXML)
-        .then(() => {
-            alert('XML Text copied to clipboard!');
-        })
-        .catch((err) => {
-            console.error('Failed to copy XML text: ', err);
-        });
+        try {
+            navigator.clipboard.writeText(concatenatedTextXML)
+            setCopyXMLState(true)
+        }
+        catch (error) {
+            console.log(error)
+        }
+
+
     }
     return (<>
         <div className="overflow-auto w-full h-screen flex flex-col justify-between p-5 bg-orange-100 rounded-lg shadow-md items-start md:flex-row">
@@ -75,11 +80,18 @@ const PromptConstructorPage = () => {
             <div className="w-full h-full md:w-1/2 text-wrap">
                 <h3 className="w-full block font-bold text-yellow-700">Concatenated Text:</h3>
                 <pre className="mt-1 p-4 bg-gray-200 border border-gray-300 rounded text-brown-700 text-wrap">{concatenatedText}</pre>
-                <button className="block mt-4 p-2 px-4 bg-[#e5d2c3] text-[#6d5d52] rounded-md cursor-pointer transition-colors duration-300" onClick={copyToClipboard}>Copy to Clipboard</button>
+                <div className="flex flex-row">
+                    <button className="block mt-4 p-2 px-4 bg-[#e5d2c3] text-[#6d5d52] rounded-md cursor-pointer transition-colors duration-300" onClick={copyToClipboard}>Copy to Clipboard</button>
+                    {copyState ? <label className="block ml-4 mt-4 p-2 px-4 text-yellow-700">Copied to Clipboard</label> : <></>}
+                </div>
 
-                <h3 className="w-full block mt-8 font-bold text-yellow-700">Concatenated Text (XML):</h3>
+
+                <h3 className="w-full block mt-10 font-bold text-yellow-700">Concatenated Text (XML):</h3>
                 <pre className="mt-1 p-4 bg-gray-200 border border-gray-300 rounded text-brown-700 text-wrap">{concatenatedTextXML}</pre>
-                <button className="block mt-4 p-2 px-4 bg-[#e5d2c3] text-[#6d5d52] rounded-md cursor-pointer transition-colors duration-300" onClick={copyXMLToClipboard}>Copy XML to Clipboard</button>
+                <div className="flex flex-row mb-8">
+                    <button className="block mt-4 p-2 px-4 bg-[#e5d2c3] text-[#6d5d52] rounded-md cursor-pointer transition-colors duration-300" onClick={copyXMLToClipboard}>Copy XML to Clipboard</button>
+                    {copyXMLState ? <label className="block ml-4 mt-4 p-2 px-4 text-yellow-700">Copied XML to Clipboard</label> : <></>}
+                </div>
             </div>
         </div>
     </>
